59d540c949782ed72a8acfcf6453d94812c65b59
Jhone-cmd <jhone.1941@gmail.com>
2026-07-20 16:12:34 -0300

    add animaldto

---
+++ src/main/java/com/jhonecmd/petland/dto/AnimalDTO.java
@@ -0,0 +1,19 @@
+package com.jhonecmd.petland.dto;
+
+import com.jhonecmd.petland.model.animal.AnimalSpecie;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class AnimalDTO {
+
+    private String name;
+    private String description;
+    private String specie;
+    private LocalDate birthday;
+
+}
