5072a422072d22d2d68c0f39d79a707dd1caa8c4
Jhone-cmd <jhone.1941@gmail.com>
2026-07-21 09:29:35 -0300

    add dto productAndservice

---
+++ src/main/java/com/jhonecmd/petland/dto/ProductAndServiceDTO.java
@@ -0,0 +1,12 @@
+package com.jhonecmd.petland.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductAndServiceDTO {
+
+    private String name;
+    private Double price;
+    private Boolean service;
+
+}
