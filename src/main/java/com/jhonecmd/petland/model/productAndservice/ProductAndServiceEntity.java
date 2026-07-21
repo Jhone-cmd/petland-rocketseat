0727ad55382707dcb92ae67379bb49955a37f912
Jhone-cmd <jhone.1941@gmail.com>
2026-07-21 09:17:18 -0300

    add entity productAndservice

---
+++ src/main/java/com/jhonecmd/petland/model/productAndservice/ProductAndServiceEntity.java
@@ -0,0 +1,45 @@
+package com.jhonecmd.petland.model.productAndservice;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product_service")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductAndServiceEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+    private Double price;
+    private Boolean service;
+
+    @Column(name = "create_at")
+    private LocalDateTime createAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
