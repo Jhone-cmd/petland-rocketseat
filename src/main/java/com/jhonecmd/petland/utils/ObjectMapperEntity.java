ea390f381d65a989cc256e4de1f2e66c88f98e0c
Jhone-cmd <jhone.1941@gmail.com>
2026-07-22 09:47:30 -0300

    creating utilities for attribute mapping and persistence

---
+++ src/main/java/com/jhonecmd/petland/utils/ObjectMapperEntity.java
@@ -0,0 +1,36 @@
+package com.jhonecmd.petland.utils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ObjectMapperEntity {
+
+    public <S, T> void copyNonNullProperties(S source, T target) {
+        if (source == null || target == null) {
+            return;
+        }
+        BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
+    }
+
+    private String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+
+        return emptyNames.toArray(new String[0]);
+    }
+}
+
