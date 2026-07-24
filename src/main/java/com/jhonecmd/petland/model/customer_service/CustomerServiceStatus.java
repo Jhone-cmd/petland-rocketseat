5c6a5c859bea43c5eb2f20e1016b59e4ce622298
Jhone-cmd <jhone.1941@gmail.com>
2026-07-24 11:18:30 -0300

    add enums status and types for customer_service

---
+++ src/main/java/com/jhonecmd/petland/model/customer_service/CustomerServiceStatus.java
@@ -0,0 +1,8 @@
+package com.jhonecmd.petland.model.customer_service;
+
+public enum CustomerServiceStatus {
+    SCHEDULED,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
---
+++ src/main/java/com/jhonecmd/petland/model/customer_service/CustomerServiceType.java
@@ -0,0 +1,8 @@
+package com.jhonecmd.petland.model.customer_service;
+
+public enum CustomerServiceType {
+    CONSULTATION,
+    EXAMINATION,
+    SANITIZATION,
+    VACCINATION
+}
